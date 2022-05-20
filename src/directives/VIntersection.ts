a1a36de23c4d5f61f480914cd4a9f2949b749e6c
Lineez <qewqew26@gmail.com>
2022-05-20 17:16:18 +0300

    default

---
+++ src/directives/VIntersection.ts
@@ -0,0 +1,15 @@
+export default {
+    name: "intersection",
+    mounted(el: HTMLElement, binding: any) {
+        new IntersectionObserver(callback.bind(this), {
+            rootMargin: "0px",
+            threshold: 1.0,
+        }).observe(el);
+
+        function callback(entries: Array<IntersectionObserverEntry>) {
+            if (entries[0].isIntersecting) {
+                binding.value();
+            }
+        }
+    },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import components from "@/components/ui";
+import directives from "@/directives/index";
+import router from "@/router/index";
+import store from "@/store";
+// global mixin & variables
+import "@/assets/scss/default/style.scss";
+// global fonts
+import "@/assets/fonts/Montserrat/style.css";
+import "@/assets/fonts/Lato/style.css";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+// register global ui component
+components.forEach((component: any) => {
+    app.component(component.name, component);
+});
+// register global directives
+directives.forEach((directive) => {
+    app.directive(directive.name, directive);
+});
+
+app.use(router).use(store).mount("#app");
