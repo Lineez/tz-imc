ecd14da36a5a954f41f112039837ac20ff154f42
Lineez <qewqew26@gmail.com>
2022-07-31 11:06:32 +0300

    remove glabal components import & up ts types

--- src/directives/VIntersection.ts
+++ src/directives/VIntersection.ts
@@ -1,6 +1,8 @@
+import { DirectiveBinding } from "vue";
+
 export default {
     name: "intersection",
-    mounted(el: HTMLElement, binding: any) {
+    mounted(el: HTMLElement, binding: DirectiveBinding) {
         new IntersectionObserver(callback.bind(this), {
             rootMargin: "0px",
             threshold: 1.0,
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import components from "@/components/ui";
 import directives from "@/directives/index";
 import router from "@/router/index";
 
 const app = createApp(App);
 const pinia = createPinia();
 
-// register global ui component
-components.forEach((component: any) => {
-    app.component(component.name, component);
-});
 // register global directives
 directives.forEach((directive) => {
     app.directive(directive.name, directive);
