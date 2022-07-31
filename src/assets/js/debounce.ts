f50bb2a4d66936772b864c3a6dbd2a33ee98d2a3
Lineez <qewqew26@gmail.com>
2022-07-31 17:18:15 +0300

    debouce eslin fix

--- src/assets/js/debounce.ts
+++ src/assets/js/debounce.ts
@@ -1,10 +1,14 @@
-export default function debounce(f, ms) {
+export default function debounce(
+    f: FunctionStringCallback,
+    ms: number,
+    ...args: any
+) {
     let inProcess = false;
 
     return function () {
         if (inProcess) return;
 
-        f.apply(this, arguments);
+        f.apply(this, args);
         inProcess = true;
 
         setTimeout(() => (inProcess = false), ms);
