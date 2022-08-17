6e6b0344fe39cd86a30ac3c5785de4f93cea30b5
Lineez <qewqew26@gmail.com>
2022-08-17 11:56:28 +0300

    api add create & change update methods

---
+++ src/api/constructor.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+type ID = number | string;
+
+export class API_Constructor {
+  constructor(private url: URL) {}
+
+  public create(data: any, options?: AxiosRequestConfig) {
+    return this.fetch({
+      ...options,
+      data,
+      method: "post",
+      url: this.url.href,
+    });
+  }
+
+  public getAll(options?: AxiosRequestConfig) {
+    return this.fetch({
+      ...options,
+      method: "get",
+      url: this.url.href,
+    });
+  }
+
+  public getById(id: ID, options?: AxiosRequestConfig) {
+    return this.fetch({
+      ...options,
+      method: "get",
+      url: this.url.href + "/" + id,
+    });
+  }
+
+  public update(id: ID, data: any, options?: AxiosRequestConfig) {
+    return this.fetch({
+      ...options,
+      data,
+      method: "put",
+      url: this.url.href + "/" + id,
+    });
+  }
+
+  public delete(id: ID, options?: AxiosRequestConfig) {
+    return this.fetch({
+      ...options,
+      method: "delete",
+      url: this.url.href + "/" + id,
+    });
+  }
+
+  private fetch(config: AxiosRequestConfig) {
+    try {
+      return axios(config);
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+}
