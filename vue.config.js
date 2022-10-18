4f12971675ffc678e4d63a3e538c9cbc1a8fd2bc
Lineez <qewqew26@gmail.com>
2022-10-18 13:31:30 +0300

    update vue config for mixins & add warning

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
+        // this includes in each file
+        // don't include not removable scss
+        // only for mixins, functions and scss variables
         additionalData: `
                     @import "@/assets/scss/default/_mixin.scss";
                 `,
